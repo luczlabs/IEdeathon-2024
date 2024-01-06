aeb49d4da4232270453e920e73cbaa653c00b7ff
Val Eltagonde <valeltagonde@gmail.com>
2024-01-06 18:09:23 +0800

    landing page before donate

--- Public/Home.py
+++ Public/Home.py
@@ -1,16 +1,104 @@
+# Packages
 import streamlit as st
 import pandas as pd
+from streamlit_extras.metric_cards import style_metric_cards
+from streamlit_extras.add_vertical_space import add_vertical_space
 
-st.title('Landing Page')
+
+# Imported
+from data import landing_page_impact, text, images
+
+
+# Variables
+ss = st.session_state
+main_color = '#4169E1'
+section_text = text['Landing Page']
+section_images = images['Landing Page']
+
+
+# Initialize
+st.set_page_config(page_title='Project Blue', page_icon='💙', layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+
+# Background
+background_image = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main \u007b
+    background-image: url('{section_images['background_image']}');
+    background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
+    background-position: center;  
+    background-repeat: no-repeat;
+\u007d
+</style>
+"""
+
+# st.markdown(background_image, unsafe_allow_html=True)
+
+
+# Title
+st.caption('THIS IS PROJECT BLUE')
+add_vertical_space(1)
+st.markdown(f"""
+    <div style="line-height:450%;">
+        <span style=" font-size:100px ; color:#31333F ; font-weight:bold; ">More than </span>
+        <br>
+        <span style=" font-size:100px ; color:{main_color} ; font-weight:bold; ">cleanups</span>
+        <span style=" font-size:100px ; color:#31333F ; font-weight:bold; ">.</span>
+    </div>""",
+    unsafe_allow_html=True
+)
+add_vertical_space(1)
+
 
 # Information about the organization
-st.header('Information')
+st.write(f"{section_text['org_info']}")
+
+col1, col2 = st.columns(2)
+with col1:
+    with st.expander(label='OUR STORY', expanded=False):
+        st.write(section_text['org_background'])
+with col2:
+    with st.expander(label='OUR SLOGAN'):
+        st.markdown(f"**:blue[MORE THAN CLEANUPS.]**\n\n{section_text['slogan_info']}")
+    
+add_vertical_space(2)
+
+# Impact by numbers
+st.caption('IMPACT BY NUMBERS')
+col1, col2, col3 = st.columns(3)
+col4, col5, col6 = st.columns(3)
+row1, row2 = [col1, col2, col3], [col4, col5, col6]
+for ind, col in enumerate(row1):
+    col.metric(label=landing_page_impact[ind][0], value=landing_page_impact[ind][1], delta='Lorem Ipsum')
+for ind, col in enumerate(row2):
+    col.metric(label=landing_page_impact[ind+3][0], value=landing_page_impact[ind+3][1], delta='Lorem Ipsum')
+
+style_metric_cards(border_left_color=main_color, border_radius_px=7, box_shadow=False)
+add_vertical_space(1)
+
+
+# Features
+st.caption('AS FEATURED IN')
+# to_be_added
+add_vertical_space(2)
+
 
 # Member benefits
-st.header('What\'s in it for you?')
+st.caption('MEMBER BENEFITS')
+col1, col2 = st.columns(2)
+with col1:
+    with st.container(border=True):
+        st.radio('What are you most excited about?', section_text['benefits'].keys(), key='benefit')
+with col2:
+    for k,v in section_text['benefits'][ss.benefit].items():
+        with st.expander(k, expanded=False):
+            st.write(v)
 
-# Call to action
-donate = st.button('Donate now', type='primary')
+add_vertical_space(2)
+
+
+# Donate
+
 apply = st.button('Apply now', type='primary')
 
 # Sponsors and partners
---
+++ Public/data.py
@@ -0,0 +1,51 @@
+text = {
+    'Landing Page' : {
+        'org_info' : 'Project Blue (PB) is a youth-led nonprofit organization that focuses on raising awareness and providing sustainable solutions on ocean pollution and marine conservation.',
+        'org_background' : 'Project Blue started as a cleanup idea that turned into an organization after the realization that cleanups aren’t enough and that we have to be more sustainable. We do a mix of physical and online projects that advocate for the oceans. It was founded in the province of Ilocos last December 2020.',
+        'slogan' : 'More than cleanups',
+        'slogan_info' : 'Raising awareness and providing sustainable solutions on ocean pollution and marine conservation in the Philippines.',
+
+        'benefits_text' : 'Become a Project Blue member today and unlock a world of opportunities to make a lasting impact on the environment while advancing your personal and professional growth!',
+        'benefits' : {
+            "✨ Impactful Contribution": {
+                "Engage in Meaningful Projects": "Actively contribute to hands-on projects focused on environmental conservation, floating fish cages, life vests, and more.",
+                "See Your Impact": "Witness the tangible results of your efforts as Project Blue works towards creating a positive and lasting impact on the environment."
+            },
+            "🗣️ Skill Development and Networking": {
+                "Professional Growth": "Access skill development opportunities through workshops, training sessions, and leadership roles within your chosen committee.",
+                "Build a Network": "Connect with like-minded individuals, professionals, and key stakeholders, fostering valuable relationships for personal and career development."
+            },
+            "🫂 Inclusive Community": {
+                "Diverse and Inclusive": "Join a diverse team that welcomes individuals from various backgrounds, including students and professionals, creating an inclusive and supportive community.",
+                "Shared Values": "Be part of a dedicated team united by a common goal of environmental conservation, fostering collaboration and camaraderie."
+            },
+            "🎖️ Recognition and Appreciation": {
+                "Certification of Commitment": "Receive official acknowledgment and certification for your commitment to Project Blue's mission as a certified changemaker.",
+                "Public Recognition": "Enjoy public recognition for your contributions through social media shout-outs, newsletters, and acknowledgment at Project Blue events."
+            },
+            "🤝 Exclusive Opportunities": {
+                "Leadership Roles": "Take on leadership positions in committees, developing and showcasing your leadership skills in areas such as engineering, creative projects, and more.",
+                "Exclusive Events": "Attend exclusive events, webinars, and gatherings, gaining access to unique learning experiences and networking opportunities within the environmental conservation sector."
+            }
+        }
+
+
+    }
+}
+
+landing_page_impact = [
+    ('Farmers given livelihood', 4),
+    ('SustainaBoats donated', 15),
+    ('Bottles upcycled', 5000),
+    ('Communities reached', 12),
+    ('Mothers with PWD children empowered', 5),
+    ('Filipinos reached in speaking engagements', 13500)
+]
+
+images = {
+    'Landing Page' : {
+        'background_image' : "https://scontent.xx.fbcdn.net/v/t1.15752-9/413936376_1915767952226636_2650962183775529488_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=510075&_nc_eui2=AeGAcQHe4jcBWqO0YIbB5qrvgGtgQFmnpuOAa2BAWaem46QHNp5BsZBwH-b1qulhoH6Pm2Ph_NKhIZNVjq8x4C-E&_nc_ohc=TeoLteRW8OwAX_ng2uw&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&cb_e2o_trans=q&oh=03_AdTNeJZSwKeWWPffzg9l77t2tbcKnOQvYsA_n5szECltdQ&oe=65C07840"
+
+    }
+
+}
