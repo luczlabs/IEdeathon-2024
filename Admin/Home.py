d23480ee89737eab2c35f56cdcac281abc485b4e
Malena Riz Ballon <malenarizballon@Malenas-MacBook-Air.local>
2024-01-20 22:41:58 +0800

    test

--- Admin/Home.py
+++ Admin/Home.py
@@ -1,4 +1,16 @@
 import streamlit as st
 import pandas as pd
+from streamlit_gsheets import GSheetsConnection
+
+url = "https://docs.google.com/spreadsheets/d/1W818H_EO4ChWBsnbhZYnajeCxnsaLi2s0JWGIgbh13s/edit#gid=2044290198"
+
+url = url.replace(' ', '%20')
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read(spreadsheet=url)
 
 st.title('Project Blue Dashboard')
+
+st.write(df)
+
--- Admin/pages/2_🧑_Members.py
+++ Admin/pages/2_🧑_Members.py
@@ -3,8 +3,7 @@ import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space
 
 # Imported
-from data import text, colors, get_image, organization_structure
-
+from data import text, colors, get_image
 # Variables
 ss = st.session_state
 main_color = colors['main']
